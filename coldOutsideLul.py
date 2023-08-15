a530a04691f2ed4afee3b3a14b599a5f4b806631
Bastian B <39513916+Chenentano@users.noreply.github.com>
2023-08-15 12:05:14 +0100

    Create coldOutsideLul.py

---
+++ coldOutsideLul.py
@@ -0,0 +1,33 @@
+name = """
+SSS    L
+S   S  L
+S      L
+ SSS   L
+    S  L
+S   S  L
+ SSS   LLLLL
+"""
+
+print(name)
+
+snowman="""
+      *     *                    
+                      *          
+*         *                 *    
+     *           *     *         
+           ^^^^^^^        *      
+ *         ^^^^^^^               
+     *   ^^^^^^^^^^^    *     *  
+         *   L L   *             
+        *     -     *            
+  *       *       *         *    
+         SSS * * S               
+         * SSSSSSSS *            
+       *      O   SS  *          
+      *       O    S   *         
+     *       O        *         
+       *              *          
+           *   *   *             
+"""
+
+print(snowman)
